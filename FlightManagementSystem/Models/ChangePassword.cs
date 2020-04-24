e5549890abd4c43263dfe3833fbe7bbef12b7214
Maria Bramnik <bramnik@zahav.net.il>
2020-04-24 19:08:45 +0300

    commit

---
+++ FlightManagementSystem/Models/ChangePassword.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementSystem.Models
+{
+    public class ChangePassword
+    {
+        public string newPassword { get; set; }
+        public string oldPassword { get; set; }
+    }
+}
